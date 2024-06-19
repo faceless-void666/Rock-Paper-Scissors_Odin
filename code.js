e6c60e36435fb9058cac76214afe7438048824a3
Rita <162321842+faceless-void666@users.noreply.github.com>
2024-06-19 21:20:40 +0300

    Fix the issue that increments count during a tie
    This issue prevented the game from working properly, since it is stated
    in the assignment that there should be 5 rounds. Now it allows to user
    play 5 rounds excluding the ties. It also fixed issue with determination
    of the winner.

--- code.js
+++ code.js
@@ -1,27 +1,30 @@
 let humanScore = 0;
 let computerScore = 0;
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
+let count = 0;
+
 
 
 function getComputerChoice() {
-  let rock = "rock";
-  let paper = "paper";
-  let scissors = "scissors";
   let randomChoice = Math.floor(Math.random() * 10);
   if (randomChoice >= 0 && randomChoice <= 3) {
-    return rock;
+    return "rock";
   } else if (randomChoice >= 4 && randomChoice <= 7) {
-    return paper;
+    return "paper";
   } else {
-    return scissors;
+    return "scissors";
   }
 }
 
 
 function getHumanChoice() {
-  let activeChoice = prompt("Enter a hand signal of choice (Rock, Paper or Scissors):");
-  return activeChoice;
+  let activeChoice = prompt("Enter a hand signal of choice (Rock, Paper or Scissors):", "");
+  let humanInput;
+  if (activeChoice === null || activeChoice === "") {
+    console.log("You need to enter a value. Try again!");
+  } else {
+    humanInput = activeChoice.toLowerCase();
+  }
+   return humanInput; 
 }
 
 
@@ -30,48 +33,57 @@ function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerC
     if (computerChoice === "scissors"){
       console.log("You win! Rock beats Scissors.");
       humanScore++;
+      count++;
     } else if (computerChoice === "paper") {
       console.log("You lose! Paper beats Rock.");
       computerScore++;
+      count++;
     } else {
       console.log("It's a tie! Play again.");
+      count = count + 0;
     }
   }
   else if (humanChoice === "paper") {
     if (computerChoice === "rock") {
       console.log("You win! Paper beats Rock.");
       humanScore++;
+      count++;
     } else if (computerChoice === "scissors") {
       console.log("You lose! Scissors beat Paper.");
       computerScore++;
+      count++;
     } else {
       console.log("It's a tie! Play again.");
+      count = count + 0;
     }
   }
   else if (humanChoice === "scissors") {
     if (computerChoice === "paper") {
       console.log("You win! Scissors beat Paper.");
       humanScore++;
+      count++;
     } else if (computerChoice === "rock") {
       console.log("You lose! Rock beats Scissors.");
       computerScore++;
+      count++;
     } else {
       console.log("It's a tie! Play again.");
+      count = count + 0;
     }
   }
 }
 
 function playGame() {
-  let count;
-  for(count = 0; count < 5; count++) {
+  for(; count < 5;) {
     playRound();
   }
-  if (humanScore >= 3) {
+  if (humanScore >= 3 && computerScore <= 2) {
     console.log("Congrats! You're the winner.");
   } else {
     console.log("Oops... You lost. Better luck next time!");
   }
 }
+
 
 
 
